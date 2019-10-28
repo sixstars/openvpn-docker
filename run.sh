66730f938a591013d1f3e72629ea3e54c13262b6
hzshang <hzshang15@gmail.com>
2019-10-28 12:25:40 +0800

    update

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ first_init(){
 add_user(){
     docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full $1 nopass
     docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient $1 > $1.ovpn
-    touch ./conf/ccd/$1
+    sudo touch ./conf/ccd/$1
 }
 
 route(){
