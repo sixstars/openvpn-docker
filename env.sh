107c303b8ffdb7883a9460ef7f8588cb59562a78
hzshang <hzshang15@gmail.com>
2019-10-28 11:49:30 +0800

    first init

---
+++ env.sh
@@ -0,0 +1,6 @@
+SERVER_URL=vpn.sixstars.team
+SERVER_PORT=11194
+OPVN_SERVER=192.168.255.0/24 # don't change it
+SERVER_CONFIG=("duplicate-cn")
+
+
---
+++ run.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+#
+# vpn_init.sh
+# Copyright (C) 2019 hzshang <hzshang15@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+cd `dirname $0`
+. ./env.sh
+OVPN_DATA=`pwd`/conf
+first_init(){
+    sudo rm -rf ./conf/*
+    docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://$SERVER_URL:$SERVER_PORT -c -d
+    for i in "${SERVER_CONFIG[@]}"
+    do
+        echo $i | sudo tee -a ./conf/openvpn.conf
+    done
+    echo WTF | docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -i kylemanna/openvpn ovpn_initpki nopass
+}
+
+add_user(){
+    docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full $1 nopass
+    docker run -v $OVPN_DATA:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient $1 > $1.ovpn
+}
+
+route(){
+    echo "iroute $2 $3" | sudo tee -a ./conf/ccd/$1
+    echo "" | sudo tee -a ./conf/openvpn.conf
+    echo "# Add $1 route table" | sudo tee -a ./conf/openvpn.conf
+    echo "push \"route $2 $3\"" | sudo tee -a ./conf/openvpn.conf
+    echo "route $2 $3" | sudo tee -a ./conf/openvpn.conf
+}
+
+show_help(){
+    printf "Usage: ./run.sh cmd ...\n"
+    printf "Cmds:\n"
+    printf "    %-40s %s\n" "init" "init vpn config"
+    printf "    %-40s %s\n" "add <user>" "init vpn config"
+    printf "    %-40s %s\n" "route <user> <ip> <netmask>" "route specific ip to user"
+}
+
+main(){
+    case $1 in
+        init)
+            first_init
+            ;;
+        add)
+            if [[ ! -z "$2" ]];then
+                add_user $2
+            else
+                show_help
+            fi
+            ;;
+        route)
+            if [[ ! -z "$4" ]];then
+                route $2 $3 $4
+            else
+                show_help
+            fi
+            ;;
+        *)
+            show_help
+            ;;
+    esac
+}
+main $@
